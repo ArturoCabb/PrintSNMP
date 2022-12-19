1023dd4404688568431e88c9f8d8483c8aee3107
Arturo Caballero <arturcabbb@outlook.com>
2022-12-18 22:10:19 -0600

    Terminado
    Se realizaron los cambios finales y ya quedó :)

--- printersnmp/ListenSNMP.py
+++ printersnmp/ListenSNMP.py
@@ -1,43 +1,49 @@
-import socket
-import random
 import argparse
-from struct import pack, unpack
-from datetime import datetime as dt
 from pysnmp.entity.rfc3413.oneliner import cmdgen
-from pysnmp.proto.rfc1902 import Integer, IpAddress, OctetString
-from datetime import datetime
+import time
+import SaveData
 
-def saveFile(value):
-    date = dt.today().strftime('%Y-%m-%d')
-    dateComplete = dt.today().strftime('%Y-%m-%d %H:%M:%SS')
-    file = open(date + ".txt" ,"a+")
-    file.write(dateComplete + " : " + value)
-    file.close
+if  __name__ == '__main__':
 
-parser = argparse.ArgumentParser(description="Te input value is")
-parser.add_argument('--ip', nargs='?', help='The target ip address', required=True)
-parser.add_argument('--community', nargs='?', help='The community group', required=True)
-parser.add_argument('--oid', nargs='?', help='The object identifier of the target', required=True)
-inputArgs = parser.parse_args()
-
-while True:
-    #ip = '192.168.1.60'
-    #community = 'public'
-    #value = "1.3.6.1.2.1.1.2.0"
-    ip = inputArgs.ip
-    community = inputArgs.community
-    value = inputArgs.oid
+    parser = argparse.ArgumentParser(description="Te input value is")
+    parser.add_argument('--ip', nargs='?', help='The target ip address', required=True)
+    parser.add_argument('--communityIndex', nargs='?', help='The community index', required=False, default='public')
+    parser.add_argument('--communityGroup', nargs='?', help='The community group', required=False, default='public')
+    parser.add_argument('--oid', nargs='?', help='The object identifier of the target like 1.3.6.1.2.1.25.3.5.1', required=False, default='1.3.6.1.2.1.25.3.5.1')
+    parser.add_argument('--version', nargs='?', help='The SNMP version of the target [v1, v2, v3]', required=True)
+    inputArgs = parser.parse_args()
     
-    generator = cmdgen.CommandGenerator()
-    comm_data = cmdgen.CommunityData(community)  # 1 means version SNMP v2c
-    transport = cmdgen.UdpTransportTarget((ip, 161))
-
-    real_fun = getattr(generator, 'getCmd')
-    res = (errorIndication, errorStatus, errorIndex, varBinds) \
-        = real_fun(comm_data, transport, value)
-
-    if not errorIndication is None or errorStatus is True:
-        print("Error: %s %s %s %s" % res)
-    else:
-        print("%s" % varBinds)
-        saveFile(str(varBinds))
+    version = 1
+    if (inputArgs.version == 'v1'):
+        version = 0
+    elif (inputArgs.version == 'v2'):
+        version = 1
+    elif (inputArgs.version == 'v3'):
+        version = 2
+    
+    print("Esc to exit")
+    
+    while True:
+        errorIndication, errorStatus, errorIndex, \
+        varBindTable = cmdgen.CommandGenerator().bulkCmd(  
+                    cmdgen.CommunityData(communityIndex= inputArgs.communityIndex, communityName= inputArgs.communityGroup, mpModel= version),  
+                    cmdgen.UdpTransportTarget((inputArgs.ip, 161)),  
+                    0,
+                    25, 
+                    (inputArgs.oid), # ipAddrTable OID . This works fine. .1.3.6.1.2.1.25.3.5.1.2.1 1.3.6.1.2.1.1.1.0
+                )
+    
+        if errorIndication:
+           print(errorIndication)
+        else:
+            if errorStatus:
+                print('%s at %s\n' % (
+                    errorStatus.prettyPrint(),
+                    errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'
+                    ))
+            else:
+                for varBindTableRow in varBindTable:
+                    for name, val in varBindTableRow:
+                        SaveData.saveFile(('%s = %s\n' % (name.prettyPrint(), val.prettyPrint())))
+                        print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+        time.sleep(2)
--- printersnmp/SaveData.py
+++ printersnmp/SaveData.py
@@ -2,7 +2,8 @@ from datetime import datetime
 
 def saveFile(value):
     date = datetime.today().strftime('%Y-%m-%d')
+    dateComplete = datetime.today().strftime('%Y-%m-%d %H:%M:%S %f %p')
     file = open(date + ".txt" ,"a+")
-    file.write(value)
+    file.write(dateComplete + ' > ' + value)
     file.close
     
